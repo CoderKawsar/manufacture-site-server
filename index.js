74f3c1a2b4c200aee42cbf40bbaafeb995590bc5
Kawsar Ahmed <coder.kawsar@gmail.com>
2022-05-27 12:10:17 +0600

    reviews and tools get done

---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5000;
+
+// ------------------ Middlewares -------------------------//
+app.use(cors());
+app.use(express.json());
+
+// ------------------ Mongo DB -------------------------//
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.otylb.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const toolsCollection = client.db("manufacturer").collection("tools");
+    const reviewsCollection = client.db("manufacturer").collection("reviews");
+
+    app.get("/", (req, res) => {
+      res.send("Hello World!");
+    });
+
+    // GET tools data
+    app.get("/tools", async (req, res) => {
+      const total = parseInt(req.query.total);
+      const query = {};
+      const options = {
+        sort: { _id: -1 },
+      };
+      const cursor = toolsCollection.find(query, options);
+      let result;
+      if (total) {
+        result = await cursor.limit(total).toArray();
+      } else {
+        result = await cursor.toArray();
+      }
+      res.send(result);
+    });
+
+    // GET reviews data
+    app.get("/reviews", async (req, res) => {
+      const total = parseInt(req.query.total);
+      const query = {};
+      const options = {
+        sort: { _id: -1 },
+      };
+      const cursor = reviewsCollection.find(query, options);
+      let result;
+      if (total) {
+        result = await cursor.limit(total).toArray();
+      } else {
+        result = await cursor.toArray();
+      }
+      res.send(result);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
